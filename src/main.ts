e385c2f0eb83990a75717ee6236ee8e5137693b6
js0507dev <js0507dev@gmail.com>
2022-02-07 23:12:16 +0900

    feat: validation layer 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
@@ -8,9 +9,13 @@ import { CommonConfig, ConfigToken } from '@src/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // set middlewares
+  app.use(helmet()); // 보안관련 http header 설정 추가
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER)); // nestjs default logger를 winston으로 변경
+  app.useGlobalPipes(new ValidationPipe()); // validation layer 추가
+
   const configService: ConfigService = app.get(ConfigService);
-  app.use(helmet());
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   const commonConfig: CommonConfig = configService.get<CommonConfig>(ConfigToken.Common);
   await app.listen(commonConfig.port);
 }
